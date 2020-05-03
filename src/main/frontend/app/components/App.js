4c603710ccff7d7cf1baeeaca118d9b8be2eb765
Stephanie Garcia <StephanieGarcia@MyMacs-MBP.fios-router.home>
2020-05-03 02:37:52 -0400

    /adoptions/new renders form

--- src/main/frontend/app/components/App.js
+++ src/main/frontend/app/components/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Route, BrowserRouter, Link, Switch } from "react-router-dom"
-//import SurrenderForm from "./SurrenderForm"
+import SurrenderForm from "./SurrenderForm"
 import TypeOfPetsContainer from "./TypeOfPetsContainer"
 //import LoginPage from "./LoginPage"
 import ListPageContainer from "./ListPageContainer"
@@ -14,6 +14,7 @@ const App = props => {
        <Route exact path="/pets" component={TypeOfPetsContainer} />
        <Route exact path="/pets/:type" component={ListPageContainer} />
        <Route exact path="/pets/:type/:id" component={ShowPage}/>
+       <Route exact path="/adoptions/new" component={SurrenderForm} />
      </Switch>
     </BrowserRouter>
   )
